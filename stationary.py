9979231a6aaeef4de84ef0a40ab72d347c52341b
VIVEK KAMATH <vivekkamathofficial13@gmail.com>
2026-02-08 09:27:44 +0530

    Create stationary.py

---
+++ stationary.py
@@ -0,0 +1,12 @@
+a4_cost-float(input("Cost of A4sheet: \n"))
+pen_cost-float(input("Cost of pen:\n"))
+pencil_cost-float(input("Cost of pencil:\n"))
+eraser_cost-float(input("Cost of eraser:\n"))
+if a4_cost<0 or pen_cost<0 or pencil_cost<0 or eraser_cost<0:
+    print("Invalid input")
+else:
+  print("Item Details")
+  print(f"A4sheet:{a4_cost:.2f}")
+  print(f"Pen: {pen_cost:.2f}")
+  print(f"Pencil:{pencil_cost:.2f}")
+  print(f"Eraser: {eraser_cost:.2f}")
